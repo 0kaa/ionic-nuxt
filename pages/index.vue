69503480bb3d4f7f8b7b54e84fd226d86fabba8e
0kaa <thebestoka0@gmail.com>
2022-07-11 19:44:11 +0200

    🚀 RELEASE: Hot reload with IOS/Android simulators

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
     <ion-content class="ion-padding">
-      <h1>Hello World</h1>
+      <h1>Hello world</h1>
     </ion-content>
   </ion-page>
 </template>
